9f829ceed98a429ac4aa2a83237f58061eb267d1
iamadithedev <iusca.adrian@gmail.com>
2023-03-31 13:38:57 -0700

    removed gles2.h header file

--- glfw/examples/gears.c
+++ glfw/examples/gears.c
@@ -29,7 +29,6 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
--- glfw/examples/windows.c
+++ glfw/examples/windows.c
@@ -31,7 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv)
+int main()
 {
     int xpos, ypos, height;
     const char* description;
